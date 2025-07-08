19368edd30d3304027f0b06c07e68f95185bc493
Shah Nawaz <shuvo1824@hotmail.com>
2025-07-08 23:42:09 +0600

    chore: tcp server setup

---
+++ main.go
@@ -0,0 +1,97 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"log/slog"
+	"net"
+)
+
+const defaultListenAddr = ":5001"
+
+type Config struct {
+	ListenAddr string
+}
+
+type Server struct {
+	Config
+	peers     map[*Peer]bool
+	ln        net.Listener
+	addPeerCh chan *Peer
+	quitCh    chan struct{}
+	msgCh     chan []byte
+}
+
+func NewServer(config Config) *Server {
+	if len(config.ListenAddr) == 0 {
+		config.ListenAddr = defaultListenAddr
+	}
+
+	return &Server{
+		Config:    config,
+		peers:     make(map[*Peer]bool),
+		addPeerCh: make(chan *Peer),
+		quitCh:    make(chan struct{}),
+		msgCh:     make(chan []byte),
+	}
+}
+
+func (s *Server) Start() error {
+	ln, err := net.Listen("tcp", s.ListenAddr)
+	if err != nil {
+		return err
+	}
+	s.ln = ln
+
+	go s.loop()
+
+	slog.Info("server started", "listenAddr", s.ListenAddr)
+
+	return s.acceptLoop()
+}
+
+func (s *Server) handleRawMsg(rawMsg []byte) error {
+	fmt.Println(string(rawMsg))
+	return nil
+}
+
+func (s *Server) loop() {
+	for {
+		select {
+		case rawMsg := <-s.msgCh:
+			if err := s.handleRawMsg(rawMsg); err != nil {
+				slog.Error("error handling raw message", "err", err)
+			}
+		case <-s.quitCh:
+			return
+		case peer := <-s.addPeerCh:
+			s.peers[peer] = true
+		}
+	}
+}
+
+func (s *Server) acceptLoop() error {
+	for {
+		conn, err := s.ln.Accept()
+		if err != nil {
+			slog.Error("error accepting connection", "err", err)
+			continue
+		}
+
+		go s.handleConn(conn)
+	}
+}
+
+func (s *Server) handleConn(conn net.Conn) {
+	peer := NewPeer(conn, s.msgCh)
+	s.addPeerCh <- peer
+	slog.Info("new peer connected", "remoteAddr", conn.RemoteAddr())
+	if err := peer.readLoop(); err != nil {
+		slog.Error("error reading from peer", "err", err, "remoteAddr", conn.RemoteAddr())
+	}
+}
+
+func main() {
+	server := NewServer(Config{})
+	log.Fatal(server.Start())
+}
